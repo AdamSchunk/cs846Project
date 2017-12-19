d81a7df3c4ec21172ac159de3292dcb6b2699983
adam <adamschunk@gmail.com>
2017-12-19 08:16:07 -0500

    idk what was left

--- analyze.py
+++ analyze.py
@@ -2,8 +2,9 @@ import os
 import json
 
 import numpy as np
+import matplotlib.pyplot as plt
 
-from scipy.stats import wilcoxon, spearmanr
+from scipy.stats import wilcoxon, spearmanr, norm
 from datetime import datetime
 
 def get_senti_score(pos, neg):
@@ -82,6 +83,7 @@ def time_comparison():
 	
 	day_stats = []
 	
+	
 	days_enum = ["mon", "tues", "wed", "thurs", "fri", "sat", "sun"]
 	for i, day_scores in enumerate(weekday_scores):
 		tmp = {}
@@ -139,10 +141,29 @@ def time_comparison():
 	output_f = open("analysis/time_of_day.txt", "w")
 	output_f.write(json.dumps(hour_stats, indent=4, sort_keys=True))
 	
+def project_users(repo):
+	commits = open("parsed_commits/" + repo + ".txt", "r")
+	commits = json.load(commits)
+	users = set()
+			
+	for commit in commits:
+		users.add(commit["user_id"])
+	
+	return users
+	
+def project_duration(repo):
+	commits = open("parsed_commits/" + repo + ".txt", "r")
+	commits = json.load(commits)
+	
+	last = datetime.strptime(commits[0]["date"],  "%Y-%m-%dT%H:%M:%SZ")
+	first = datetime.strptime(commits[-1]["date"],  "%Y-%m-%dT%H:%M:%SZ")
+	
+	return last.timestamp() - first.timestamp()
+	
 def project_specific():
 	print("****PROJECT SPECIFIC COMPARISON****")
 	project_scores = []
-	stars_json = json.load(open("stars.txt"))
+	project_details = json.load(open("repoDetails.txt"))
 
 	score_means = []
 	stars = []
@@ -160,13 +181,22 @@ def project_specific():
 			
 			scores.append(get_senti_score(pos, neg))
 			
+		for p in project_details:
+			if p["repo_name"] == filename[:-4]:
+				tmp["stars"] = p["stars"]
+				stars.append(p["stars"])
+				break
+			
+		users = project_users(filename[:-4])
 		tmp["num_commits"] = len(scores)
 		tmp["mean"] = np.mean(scores)
 		tmp["std"] = np.std(scores)
-		tmp["stars"] = stars_json[filename[:-4]]
+		tmp["num_users"] = len(users)
+		tmp["project_duration"] = project_duration(filename[:-4])
+		
 		
 		score_means.append(np.mean(scores))
-		stars.append(int(stars_json[filename[:-4]]))
+		
 		project_scores.append(tmp)
 		
 	
@@ -174,9 +204,406 @@ def project_specific():
 	output_f.write(json.dumps(project_scores, indent=4, sort_keys=True))
 	
 	correlation = spearmanr(score_means, stars)
+	
+	plt.scatter(stars, score_means)
+	z = np.polyfit(stars, score_means, 1)
+	plt.xlabel("Number of Stars")
+	plt.ylabel("Average Sentiment")
+	#plt.show()
+	
+	sum = 0
+	for p in project_scores:
+		if p["num_commits"] > 731910/55:
+			print("above average")
+			sum = sum + p["num_commits"]
+		
+	print(sum)
+	#print(correlation)
+	
+def commit_distribution():
+
+	y = []
+	for filename in os.listdir("from_senti/"):
+		with open("from_senti/" + filename) as f:
+			for i, l in enumerate(f):
+				pass
+		y.append(i)
+	
+	
+	y = np.sort(y)
+	
+	print(y)
+	x = np.linspace(1, len(y), len(y))
+	plt.plot(x,y)
+	plt.savefig("analysis/commitDistribution.png")
+	plt.clf()
+	
+def sentiment_over_time(repo):
+	print("****TIME OF DAY COMPARISON****")
+	
+	senti_file = open("from_senti/" + repo + ".txt")
+
+	senti_lines = senti_file.read().splitlines()
+	base_commits = json.load(open("parsed_commits/" + filename))
+	
+	num_commits = 0
+	for commit in base_commits:
+		num_commits = num_commits + 1 
+	
+	scores = []
+	for i, commit in enumerate(base_commits):
+		splt = senti_lines[i].split("\t")
+		neg = int(splt[-1])
+		pos = int(splt[-2])
+		
+		date = datetime.strptime(commit["date"],  "%Y-%m-%dT%H:%M:%SZ")
+		
+		hour = date.hour
+		day = date.weekday()
+		
+		score = get_senti_score(pos,neg)
+		scores.append(score)
+	
+	score_avgs = []
+	for i in range(0, len(scores), len(scores)/100):
+		chunk = scores[i:i + len(scores)/100]
+		score_avgs = sum(chunk)/len(chunk)
+	
+def project_productivity():
+	print("project overall")
+	data = open("analysis/project_specific.txt")
+	data = json.load(data)
+	
+	x = []
+	y = []
+	
+	for project in data:
+		velocity = project["num_commits"]/project["project_duration"] * 1000000 / project["num_users"]
+		x.append(project["mean"])
+		y.append(velocity)
+		
+	plt.scatter(x,y)
+	plt.xlabel("average sentiment")
+	plt.ylabel("Velocity")
+	plt.savefig("analysis/velocity.png")
+	plt.clf()
+	
+	correlation = spearmanr(x, y)
+	
 	print(correlation)
+	
+	
+def project_productivity_weekly():
+	print("project weekly")
+	data = open("analysis/project_specific.txt")
+	data = json.load(data)
+	
+	x = []
+	y = []
+	
+	weekly_velocity = []
+	weekly_sentiment = []
+	for project in data:
+		project_scores = []
+		project_times = []
+		
+		senti_file = open("from_senti_non_abandoned/" + project["project"] + ".txt", "r")
+		sentis = senti_file.read().splitlines()
+		
+		for senti in sentis:
+			splt = senti.split("\t")
+			neg = int(splt[-1])
+			pos = int(splt[-2])
+			
+			project_scores.append(get_senti_score(pos,neg))
+		
+		commit_file = open("non_abandoned/" + project["project"] + ".txt", "r")
+		commits = json.load(commit_file)
+		commits.reverse()
+		project_scores.reverse()
+		for commit in commits:
+			project_times.append(datetime.strptime(commit["date"],  "%Y-%m-%dT%H:%M:%SZ").timestamp())
+		
+		
+		num_users = project["num_users"]		
+		start_time = project_times[0]
+		curr_time = project_times[0]
+		num_commits = 0
+		tmp_sentiment = []
+		for i, commit in enumerate(commits):
+			curr_time = datetime.strptime(commit["date"],  "%Y-%m-%dT%H:%M:%SZ").timestamp()
+			num_commits += 1
+			tmp_sentiment.append(project_scores[i])
+			if curr_time > start_time + 604800:
+				weekly_sentiment.append(sum(tmp_sentiment)/len(tmp_sentiment))
+				weekly_velocity.append(num_commits/num_users/50)
+
+				num_commits = 0
+				tmp_sentiment = []
+				start_time = curr_time
+	
+	x = []
+	y = []
+	for i in range(0, len(weekly_sentiment)):
+		if weekly_velocity[i] < 3:
+			x.append(weekly_sentiment[i])
+			y.append(weekly_velocity[i])
+	
+	plt.scatter(x,y,s=10)
+	plt.xlabel("average weekly sentiment")
+	plt.ylabel("Velocity")
+	plt.savefig("analysis/velocity_weekly.png")
+	plt.clf()
+	
+	correlation = spearmanr(x, y)
+	print(correlation)
+
+
+def user_productivity():
+	print("user productivity")
+	data = open("analysis/project_specific.txt")
+	data = json.load(data)
+	
+	x = []
+	y = []
+	
+	user_velocity = []
+	weekly_sentiment = []
+	
+	users = {}
+	for project in data:
+		project_scores = []
+		
+		senti_file = open("from_senti/" + project["project"] + ".txt", "r")
+		sentis = senti_file.read().splitlines()
+		
+		scores = []
+		for senti in sentis:
+			splt = senti.split("\t")
+			neg = int(splt[-1])
+			pos = int(splt[-2])
+			
+			scores.append(get_senti_score(pos,neg))
+		
+		commit_file = open("parsed_commits/" + project["project"] + ".txt", "r")
+		commits = json.load(commit_file)
+		for i, commit in enumerate(commits):
+			user = str(commit["user_id"])
+			time = datetime.strptime(commit["date"],  "%Y-%m-%dT%H:%M:%SZ").timestamp()
+			score = scores[i]
+			data = [time, score]
+			if not user in users.keys():
+				users[user] = [data]
+			else:
+				users[user].append(data)
+
+	for key, data in users.items():
+		if len(data) <= 20:
+			continue
+		
+		duration = data[0][0] - data[-1][0]
+		velocity = len(data)/duration * 100
+		scores = [d[1] for d in data]
+		avg_score = sum(scores)/len(data)
+		
+		if velocity < .0001 or velocity > .3:
+			continue
+		
+		x.append(avg_score)
+		y.append(velocity)
+	
+	
+	plt.scatter(x,y,s=10)
+	plt.xlabel("user average sentiment")
+	plt.ylabel("Velocity")
+	plt.savefig("analysis/user_sentiment.png")
+	plt.clf()
+	
+	correlation = spearmanr(x, y)
+	print(correlation)
+	
+def work_distribution():
+	print("work distribution")
+	data = open("analysis/project_specific.txt")
+	data = json.load(data)
+	
+	x = []
+	y = []
+	
+	user_velocity = []
+	weekly_sentiment = []
+	
+	for project in data:
+		users = {}
+		
+		senti_file = open("from_senti/" + project["project"] + ".txt", "r")
+		sentis = senti_file.read().splitlines()
+		
+		scores = []
+		for senti in sentis:
+			splt = senti.split("\t")
+			neg = int(splt[-1])
+			pos = int(splt[-2])
+			
+			scores.append(get_senti_score(pos,neg))
+		
+		commit_file = open("parsed_commits/" + project["project"] + ".txt", "r")
+		commits = json.load(commit_file)
+		for i, commit in enumerate(commits):
+			user = str(commit["user_id"])
+			time = datetime.strptime(commit["date"],  "%Y-%m-%dT%H:%M:%SZ").timestamp()
+			score = scores[i]
+			data = [time, score]
+			if not user in users.keys():
+				users[user] = [data]
+			else:
+				users[user].append(data)
+	
+		total_commits = len(commits)
+		for key, data in users.items():
+			if len(data) <= 20:
+				continue
+			
+			scores = [d[1] for d in data]
+			avg_score = sum(scores)/len(data)
+
+			user_commits = len(scores)
+			
+			
+			
+			x.append(avg_score)
+			y.append(user_commits/total_commits * 100)
+	
+	
+	plt.scatter(x,y,s=10)
+	plt.xlabel("user average sentiment")
+	plt.ylabel("Percent of Work")
+	plt.savefig("analysis/workload_distribution.png")
+	plt.clf()
+	
+	correlation = spearmanr(x, y)
+	print(correlation)
+	
+def normal_from_data():
+	non_abandoned_scores = []
+	for filename in os.listdir("from_senti_non_abandoned/"):
+		print(filename)
+		senti_file = open("from_senti_non_abandoned/" + filename)
+		senti_lines = senti_file.read().splitlines()
+		base_commits = json.load(open("non_abandoned/" + filename))
+		
+		for i, commit in enumerate(base_commits):
+			splt = senti_lines[i].split("\t")
+			neg = int(splt[-1])
+			pos = int(splt[-2])
+			
+			non_abandoned_scores.append(get_senti_score(pos, neg))
+	
+	data = non_abandoned_scores
+
+	# Fit a normal distribution to the data:
+	mu, std = norm.fit(data)
+
+	# Plot the histogram.
+	plt.hist(data, bins=7, normed=True, alpha=0.0, color='g')
+
+	# Plot the PDF.
+	xmin, xmax = plt.xlim()
+	x = np.linspace(xmin, xmax, 200)
+	p = norm.pdf(x, mu, std)
+	plt.plot(x, p, 'k', linewidth=2)
+	plt.xlabel("Sentiment")
+	title = "Fit results: mu = %.2f,  std = %.2f" % (mu, std)
+	plt.title(title)
+	
+def normal_from_data_abandoned():
+	non_abandoned_scores = []
+	for filename in os.listdir("from_senti"):
+		print(filename)
+		senti_file = open("from_senti/" + filename)
+		senti_lines = senti_file.read().splitlines()
+		base_commits = json.load(open("abandoned/" + filename))
+		
+		for i, commit in enumerate(base_commits):
+			splt = senti_lines[i].split("\t")
+			neg = int(splt[-1])
+			pos = int(splt[-2])
+			
+			non_abandoned_scores.append(get_senti_score(pos, neg))
+	
+	data = non_abandoned_scores
+
+	# Fit a normal distribution to the data:
+	mu, std = norm.fit(data)
+
+	# Plot the histogram.
+	plt.hist(data, bins=7, normed=True, alpha=0.0, color='g')
+
+	# Plot the PDF.
+	xmin, xmax = plt.xlim()
+	x = np.linspace(xmin, xmax, 200)
+	p = norm.pdf(x, mu, std)
+	plt.plot(x, p, 'r', linewidth=2)
+	title = "Fit results: mu = %.2f,  std = %.2f" % (mu, std)
+	plt.title(title)
+	plt.savefig("analysis/normal_non_abandoned.png")
+	plt.show()
+	
+def project_abandon_over_time():
+	print("abandon percentile")
+	data = open("analysis/project_specific.txt")
+	data = json.load(data)
+	
+	x = []
+	y = []
+	
+	weekly_velocity = []
+	weekly_sentiment = []
+	for project in data:
+		project_scores = []
+		project_times = []
+		
+		senti_file = open("from_senti_non_abandoned/" + project["project"] + ".txt", "r")
+		sentis = senti_file.read().splitlines()
+		
+		for senti in sentis:
+			splt = senti.split("\t")
+			neg = int(splt[-1])
+			pos = int(splt[-2])
+			
+			project_scores.append(get_senti_score(pos,neg))
+		if len(project_scores) < 90:
+			continue
+		commit_file = open("non_abandoned/" + project["project"] + ".txt", "r")
+		commits = json.load(commit_file)
+		commits.reverse()
+		project_scores.reverse()
+
+		project_percent_sentiment = []
+		for i, score in enumerate(project_scores):
+			if i%int(len(project_scores)/50) == 0:
+				project_percent_sentiment.append(score)
+
+				
+		weekly_sentiment.append(project_percent_sentiment)
+
+	x = np.linspace(1,48,48)
+	y = []
+	for i in range(0, 48):
+		tmp = [sent[i] for sent in weekly_sentiment]
+		y.append(sum(tmp)/len(tmp))
+	
+	plt.scatter(x,y,s=10)
+	plt.xlabel("average weekly sentiment")
+	plt.ylabel("Velocity")
+	plt.savefig("analysis/abandon_percentile.png")
+	plt.clf()
+	
+	correlation = spearmanr(x, y)
+	print(correlation)	
 	
 if __name__ == "__main__":
 	#compare_languages()
 	#time_comparison()
-	project_specific()
+	normal_from_data()
+	normal_from_data_abandoned()
--- github_interact.py
+++ github_interact.py
@@ -5,6 +5,7 @@ import time
 import datetime
 import requests
 
+
 from requests.auth import HTTPBasicAuth
 
 def authenticate():
@@ -16,12 +17,13 @@ def get_repo_list(file):
 	repo_file = open("repo_lists/" + file + ".txt", "r")
 	return repo_file.read().splitlines()
 
-def parse_commit(commits):
+def parse_commits(commits):
 	parsed = []
 	for commit in commits:
 		tmp = {}
 		
 		tmp["user_id"] = "-1"
+		tmp["username"] = "-1"
 		tmp["message"] = "-1"
 		tmp["date"] = "-1"
 		
@@ -33,12 +35,15 @@ def parse_commit(commits):
 		
 		if commit["commit"]["author"]["date"]:
 			tmp["date"] = commit["commit"]["author"]["date"]
+		
+		if commit["commit"]["author"]["name"]:
+			tmp["username"] = commit["commit"]["author"]["name"]
 			
 		parsed.append(tmp)
 	return parsed
 
 def save_repo_commits(repo, output_loc):
-	completed_repos = os.listdir("commit_statements")
+	completed_repos = os.listdir("parsed_commits")
 	if (repo.replace("/","_") + ".txt" in completed_repos):
 		print(repo + " already finished")
 		return
@@ -50,10 +55,13 @@ def save_repo_commits(repo, output_loc):
 	page = re.findall('page=\d{1,}', header)[1]
 	num_pages = int(page[page.index("=")+1:])
 	
+	
+	
 	parsed_commits = []
+	print(num_pages)
 	for i in range(1,num_pages):
-		if(i%int(num_pages/20) == 0):
-			print("page " + str(i) + " of " + str(num_pages))
+		#if(i%int(num_pages/20) == 0):
+		#	print("page " + str(i) + " of " + str(num_pages))
 		tail = "?client_id=7295f7c92c892216de7b&client_secret=388edda4532c9499dbe16b78f876e76883e54fc8&page=" + str(i)
 		r = requests.get('https://api.github.com/repos/' + repo + '/commits' + tail)
 		requests_left = int(r.headers["X-RateLimit-Remaining"])
@@ -63,28 +71,40 @@ def save_repo_commits(repo, output_loc):
 		
 		if(r.ok):
 			commits = json.loads(r.text or r.content)
-			parsed_commits.append(parse_commit(commits))
-	
+			tmp = parse_commits(commits)
+			for t in tmp:
+				parsed_commits.append(t)
 	output_file.write(json.dumps(parsed_commits, indent=4, sort_keys=True))
 	
-def get_repo_stars():
-	star_list = {}
-	output_file = open("stars.txt", "w")
-	lists = ["c", "java", "c++", "ruby", "python", "main"]
+def get_repo_data():
+	data = []
+	output_file = open("repoDetails.txt", "w")
+	#lists = ["c", "java", "c++", "ruby", "python", "main"]
+	lists = ["all", "abandoned"]
 	for l in lists:
 		repo_list = get_repo_list(l)
 		for repo in repo_list:
+			tmp = {}
 			print(repo)
+			
 			r = requests.get('https://api.github.com/repos/' + repo + '?client_id=7295f7c92c892216de7b&client_secret=388edda4532c9499dbe16b78f876e76883e54fc8')
 			r_json = json.loads(r.text)
-			star_list[repo.replace("/","_")] = r_json["stargazers_count"]
+			tmp["repo_name"] = repo.replace("/","_")
+			tmp["language"] = r_json["language"]
+			tmp["size"] = r_json["size"]
+			tmp["stars"] = r_json["stargazers_count"] 
+			tmp["abandoned"] = (l == "abandoned")
 			
-	output_file.write(json.dumps(star_list, indent=4, sort_keys=True))
+			
+			data.append(tmp)
+			
+	output_file.write(json.dumps(data, indent=4, sort_keys=True))
 			
 def run():
 	authenticate()
-	output_dir = "commit_statements/"
-	lists = ["c", "java", "c++", "ruby", "python", "main"]
+	output_dir = "parsed_commits/"
+	#lists = ["c", "java", "c++", "ruby", "python", "main", "abandoned"]
+	lists = ["abandoned"]
 	for l in lists:
 		repo_list = get_repo_list(l)
 		for repo in repo_list:
@@ -93,7 +113,32 @@ def run():
 			except:
 				print(repo + " did not finish")
 				continue
-	
+
+def get_user_locations():
+	lists = ["all"]
+	locations = set()
+	for l in lists:
+		repo_list = get_repo_list(l)
+		for repo in repo_list:
+			commit_file = open("parsed_commits/" + repo.replace("/","_") + ".txt", "r")
+			commits = json.load(commit_file)
+			users = set()
+			
+			for commit in commits:
+				users.add(commit["user_id"])
+			
+			for user in users:
+			
+				r = requests.get('https://api.github.com/users/' + "Eli" + '?client_id=7295f7c92c892216de7b&client_secret=388edda4532c9499dbe16b78f876e76883e54fc8')
+				r_json = json.loads(r.text)
+				print(r_json)
+				if r_json["location"]:
+					loc = r_json["location"]
+					locations.add(loc)
+			
+	output_file.write(json.dumps(locations, indent=4, sort_keys=True))
+				
 if __name__ == "__main__":
-	#run()
-	get_repo_stars()
+	run()
+	#get_repo_data() # run this after to set the number of stars.
+	#get_user_locations()
